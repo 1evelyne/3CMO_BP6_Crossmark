9774bff4edfcf1a90655f27df3cfa920da61a33f
1evelyne <Evelyne@MacBook-Pro-4.local>
2017-03-28 09:21:28 +0200

    Referenties beter gestyled, responsiveness verbeterd

--- js/app.js
+++ js/app.js
@@ -2,4 +2,41 @@ $(document).ready(function(){
     $("button").click(function(){
         $(".teambox").slideToggle();
     });
+
+/*REFERENTIES*/
+    $('.main-iso').isotope({
+  itemSelector: '.item',
+  layoutMode: 'fitRows'
+});
+// Isotope klik functie
+$('.iso-nav ul li').click(function() {
+  $('.iso-nav ul li').removeClass('active');
+  $(this).addClass('active');
+
+  var selector = $(this).attr('data-filter');
+  $('.main-iso').isotope({
+    filter: selector
+  });
+  return false;
+});
+//wrapper
+var $wrapper = $('.wrapper').isotope({
+  itemSelector: '.item',
+  percentPosition: true,
+  masonry: {
+    columnWidth: '.grid-sizer'
+  }
+});
+$wrapper.imagesLoaded().progress(function() {
+  $wrapper.isotope('layout');
+});
+
+/*$('.grid').isotope({
+  itemSelector: '.grid-item',
+  masonry: {
+    columnWidth: 100
+  }
+});*/
+
+
 });
