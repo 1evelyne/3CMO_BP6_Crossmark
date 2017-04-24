91f8fcbcf2aaa019788b30f87f73e6dfaf7e2279
1evelyne <Evelyne@MacBook-Pro-4.local>
2017-04-24 14:52:05 +0200

    Grid testing Referenties
    Added masonry grid

--- js/app.js
+++ js/app.js
@@ -44,6 +44,31 @@ $wrapper.imagesLoaded().progress(function() {
     columnWidth: 100
   }
 });*/
+});
+/*REFERENTIES GRID*/
+$( function() {
+  var $referentiegrid = $('#referentiegrid');
+  $referentiegrid.isotope({
+    masonry: {columnWidth: 70},
+    masonryHorizontal: {rowHeight: 70}
+  });
+  var isVertical = true;
+  $('#toggle').click( function() {
+    isVertical = !isVertical;
+    // set container size styles
+    var sizeStyle = isVertical ?
+      { width: '100%', height: $referentiegrid.height() } :
+      { height: '80%', width: $referentiegrid.width() };
+    // disable transition styles for a second
+    $referentiegrid.addClass('no-transition').css( sizeStyle );
+    // trigger redraw, see http://blog.alexmaccaw.com/css-transitions
+    var redraw = $referentiegrid[0].offsetHeight;
+    // re-enable transition and trigger different layoutMode
+    $referentiegrid.removeClass('no-transition')
+      .isotope({
+        layoutMode: isVertical ? 'masonry' : 'masonryHorizontal'
+      });
 
+  });
 
 });
